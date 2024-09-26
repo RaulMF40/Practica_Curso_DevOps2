2b77bf1447079c3556056c80f346e48a04264a51
RaulMF40 <raulmf.30@gmail.com>
2024-09-26 19:33:25 +0200

    updated flyio2

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'] // Asegúrate de incluir las extensiones necesarias
-}
